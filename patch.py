cfa324f51edd4c441e260e34122a53a057722d6a
Ella Rose <erose1337@hotmail.com>
2015-09-04 21:47:20 -0700

    module wrappers, sys.stdout log

--- patch.py
+++ patch.py
@@ -1,3 +1,4 @@
+import sys
 import importlib
 
 import mpre.base
@@ -10,6 +11,7 @@ class Module_Wrapper(mpre.base.Wrapper):
     def __init__(self, **kwargs):
         super(Module_Wrapper, self).__init__(**kwargs)
         self.wraps(importlib.import_module(self.module_name))
+        sys.modules[self.module_name] = self
         
  
 class File_Logger(mpre.base.Wrapper):
@@ -20,26 +22,27 @@ class File_Logger(mpre.base.Wrapper):
     
     def __init__(self, **kwargs):
         super(File_Logger, self).__init__(**kwargs)
-        self.wraps(self.file)
         self.log = self.create(self.log_type)
         
     def write(self, data):
         self.log.write(data)
         self.file.write(data)
+        self.file.flush()
         
         
 class sys_Wrapper(Module_Wrapper):
             
     defaults = Module_Wrapper.defaults.copy()
+    defaults.update({"module_name" : "sys"})
     
     def _get_stdout(self):
         return self.wrapped_object.stdout
     def _set_stdout(self, value):
-        try:
-            mpre.objects[self._last_log].delete()
-        except AttributeError:
-            pass
-        file_logger = self.wrapped_object.stdout = File_Logger(file=value)
-        self._last_log = file_logger.instance_name
+        self._logger.wraps(value) 
+        self.wrapped_object.stdout = self._logger        
     stdout = property(_get_stdout, _set_stdout)               
     
+    def __init__(self, **kwargs):
+        super(sys_Wrapper, self).__init__(**kwargs)
+        self._logger = File_Logger(file=sys.stdout)
+        sys.stdout = self._logger
