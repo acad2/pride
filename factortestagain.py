0c2a6e856c244601eab3d7de503eb169391a74c8
Ella Rose <erose1337@hotmail.com>
2015-12-13 13:40:34 -0800

    unit test for factoring stuff

--- factortestagain.py
+++ factortestagain.py
@@ -58,9 +58,9 @@ def factor2(number):
             # do an exponential search for the exponent for this prime
             exponent_scaler = 2
             while not number % prime:
-                prime_exponent *= exponent_scaler
-      #          print "Testing exponent: ", prime, prime_exponent
+                #          print "Testing exponent: ", prime, prime_exponent
                 number, remainder = divmod(number, (prime ** prime_exponent))
+                prime_exponent *= exponent_scaler
                 if not number:
                     break
                 if remainder:
@@ -71,7 +71,7 @@ def factor2(number):
                         prime_exponent += 1
                         number, remainder = divmod(number, prime)
                     break
-                    
+                exponent_scaler *= 2    
             factors.append((prime, prime_exponent))
         if prime >= original_number or not number:
             break        
@@ -85,24 +85,27 @@ def factor3(number):
     factors = []
     original_number = number
     prime = prime_scalar = 1
+    last_remainder = 0
     while True:
         prime += prime_scalar
+    #    print "{} / {} = {}".format(number, prime, divmod(number, prime))
         _number, remainder = divmod(number, prime)
         prime_scalar *= 2
         if _number == 1:
             prime += remainder
             _number, remainder = 1, 0
-            prime_scalar = 1
+            
         if not remainder:
             number = _number
-            prime_exponent = 1
+            prime_scalar = prime_exponent = 1
             
             # do an exponential search for the exponent for this prime
             exponent_scaler = 2
             while not number % prime:
-                prime_exponent *= exponent_scaler
-      #          print "Testing exponent: ", prime, prime_exponent
+                
+   #             print "Testing exponent: ", prime, prime_exponent
                 number, remainder = divmod(number, (prime ** prime_exponent))
+                prime_exponent *= exponent_scaler
                 if not number:
                     break
                 if remainder:
@@ -115,14 +118,39 @@ def factor3(number):
                     break
                     
             factors.append((prime, prime_exponent))
+
         if prime >= original_number or not number:
-            break        
+            break      
+        if remainder > last_remainder and (last_remainder and remainder): # skipped a factor
+     #       print "Skipped a factor; remainder: {}; last remainder: {}".format(remainder, last_remainder)
+            prime -= prime_scalar / 2
+            prime_scalar = 1
+    #        print "Skipped a factor, rolling back", prime, prime_scalar
+            last_remainder = 0
+        else:
+            last_remainder = remainder            
     return factors      
+    
+def calculate(factors):
+    calculated = 1
+    for prime, exponent in factors:
+        calculated *= pow(prime, exponent)
+    return calculated
+    
+def test_factor(factoring_algorithm, number):
+    assert calculate(factoring_algorithm(number)) == number
     
 if __name__ == "__main__":
     N = int(''.join(str(ord(char)) for char in 
         """MIGHAoGBAN01lQw6DEtRySBBm+Sxl2ivcYmNB6UHovD1m4JOzZKXdHSg/V2S8j5q
         8nb42Up17iYluPqTBxJH49JzoRqc3lGN4QtiSgQYI0DK9dkYlUIJcqdlYtcefhHH
         w7hXtOHOTDx6ffAZaIx8j2BlmtQAZHqSBXRp0Uy4xPyfXMHdbP0DAgEC"""))
-    print Timed(factor3, 1)(N)
-    print Timed(factor3, 1)((2 ** 4096) - 1024)
+    #print factor3(N)#Timed(factor3, 1)(N)
+    #answer = factor3(2 ** 4096 - 1024)#Timed(factor3, 1)((2 ** 4096) - 1024)
+    #calculated = 1
+    #for prime, exponent in answer:
+    #    calculated *= pow(prime, exponent)
+    #assert calculated == (2 ** 4096 - 1024), (calculated, "\n\n", (2 ** 4096 - 1024))
+  #  print factor2(100)
+    test_factor(factor3, 1012308971230)
+    print Timed(factor3, 1)(1012308971230)
