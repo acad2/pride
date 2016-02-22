73fd66643217c67b6daf8ec4dbbb61611d8b651d
Ella Rose <erose1337@hotmail.com>
2016-02-22 14:57:48 -0800

    fixed ciphertest diffusion

--- crypto/ciphertest.py
+++ crypto/ciphertest.py
@@ -12,69 +12,54 @@ for number in range(256):
     INVERSE_S_BOX[output] = number
     
 def block_rotation(input_bytes):    
-    bits = cast(input_bytes, "binary")        
-    return bytearray(int(bits[index::8], 2) for index in range(8))
+    bits = cast(bytes(input_bytes), "binary")    
+    for index, byte in enumerate(int(bits[index::8], 2) for index in range(8)):
+        input_bytes[index] = byte    
     
-def derive_round_key(key, key_xor):           
-    for index, key_byte in enumerate(key):        
-        key[index] = S_BOX[index ^ key_xor ^ key_byte]           
-                   
-def substitution(input_bytes, xor_sum, indices):        
-    for index in indices:        
-        xor_sum ^= input_bytes[index]# input_bytes[index] # remove the current byte from the xor sum         
-        input_bytes[index] ^= S_BOX[xor_sum] # generate a psuedorandom byte from everything but the current byte + combine with current byte
-        xor_sum ^= input_bytes[index] # include byte XOR psuedorandom_byte in the xor sum         
-       
-def process_message(data, indices):
-    xor_sum = 0
+def xor_sum(data):
+    _xor_sum = 0
     for byte in data:
-        xor_sum ^= byte
+        _xor_sum ^= byte
+    return _xor_sum
     
-    for index in indices:
-        xor_sum ^= data[index]
-        data[index] ^= S_BOX[index ^ xor_sum]
-        xor_sum ^= data[index]
-
+def derive_round_key(key):    
+    xor_sum_of_key = xor_sum(key)
+    for index, key_byte in enumerate(key):        
+        key[index] = S_BOX[index ^ xor_sum_of_key ^ key_byte]           
+    block_rotation(key)
+    
+def substitution(input_bytes, xor_sum_of_data, indices):            
+    for index in indices:        
+        xor_sum_of_data ^= input_bytes[index]# input_bytes[index] # remove the current byte from the xor sum               
+        input_bytes[index] ^= S_BOX[xor_sum_of_data] # generate a psuedorandom byte from everything but the current byte + combine with current byte
+        xor_sum_of_data ^= input_bytes[index] # include byte XOR psuedorandom_byte in the xor sum         
+       
+def xor_with_key(data, key):    
+    for index, byte in enumerate(key):
+        data[index] ^= byte    
+        
 def encrypt(plaintext, key):  
-    indices = INDICES[len(key)] # (0, 1, 2, ..., N) where N = length of key or the blocksize
-    key = bytearray(key)
-    plaintext = block_rotation(plaintext)  
-    key_xor = 0
-    data_xor = 0
-    for index, key_byte in enumerate(key):
-        key_xor ^= key_byte
-        data_xor ^= plaintext[index]
-        
-    derive_round_key(key, key_xor)           
-    process_message(plaintext, indices)
-    
-    xor_sum = 0 
-    for index, data_byte in enumerate(plaintext):               
-        xor_sum ^= data_byte ^ key[index]
-    substitution(plaintext, xor_sum, indices)    
-    return bytes(plaintext)
-    
-def decrypt(ciphertext, key):               
-    indices = REVERSE_INDICES[len(key)]
-    key = bytearray(key)
-    key_xor = 0
-    for index, key_byte in enumerate(key):
-        key_xor ^= key_byte
-        
-    derive_round_key(key, key_xor)  
-    ciphertext = bytearray(ciphertext) 
-    xor_sum = 0          
-    for index, key_byte in enumerate(key):               
-        xor_sum ^= ciphertext[index] ^ key_byte
-    substitution(ciphertext, xor_sum, indices)  
+    # indices = INDICES[len(key)] # (0, 1, 2, ..., N) where N = length of key or the blocksize    
+    return crypt_block(plaintext, key, INDICES[len(key)])
 
-    process_message(ciphertext, indices)
-    return bytes(block_rotation(bytes(ciphertext)))
+def decrypt(ciphertext, key):    
+    return crypt_block(ciphertext, key, REVERSE_INDICES[len(key)])
+    
+def crypt_block(data, key, indices):    
+    key = bytearray(key)
+    data = bytearray(data)
+    
+    derive_round_key(key)                
+    xor_with_key(data, key)
+    substitution(data, xor_sum(data), indices)           
+    xor_with_key(data, key)
+   
+    return bytes(data)
     
 def test_encrypt_decrypt():
     data = "\x00" * 7
     key = ("\x00" * 7) + "\x00"
-    for count in range(256):
+    for count in range(5):
         _data = data + chr(count)
         ciphertext = encrypt(_data, key)    
         plaintext = decrypt(ciphertext, key)
