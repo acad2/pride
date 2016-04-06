5b963d55deac35a20ec6454182eb6a47010a64c3
Ella Rose <erose1337@hotmail.com>
2016-04-05 23:06:31 -0700

    improved messenger

--- gui/messenger.py
+++ gui/messenger.py
@@ -5,20 +5,38 @@ import pride.datatransfer
 
 class Client(pride.datatransfer.Data_Transfer_Client):
     
-    pass
+    def receive(self, messages):
+        for sender, message in messages:
+            self.parent.receive_message(sender, message)            
             
-              
+             
+class Contact_Button(pride.gui.gui.Button): 
+
+    def left_click(self, mouse):    
+        self.parent_application.current_contact = self.text
+        
+    
 class Popup_Menu(pride.gui.gui.Window):
                 
-    defaults = {"pack_mode" : "popup_menu"}
+    defaults = {"pack_mode" : "popup_menu", "background_color" : (125, 125, 125, 255),
+                "h_range" : (0, 300), "w_range" : (0, 400)}
     
-    
+    def __init__(self, **kwargs):
+        super(Popup_Menu, self).__init__(**kwargs)
+        self.create("pride.gui.widgetlibrary.Dialog_Box", text=self.text,
+                    callback=(self.reference, "handle_input"))
+        
+    def handle_input(self, text):
+        self.parent_application.contacts.create(Contact_Button, text=text)
+        self.delete()
+        
+        
 class Add_Contact_Button(pride.gui.gui.Button):     
 
     defaults = {"text" : "add contact..."}
     
     def left_click(self, mouse):
-        self.parent_application.create(Popup_Menu)
+        self.parent_application.create(Popup_Menu, text="Enter the contacts username: ")
         self.parent_application.pack()
         
          
@@ -36,7 +54,7 @@ class Messenger(pride.gui.gui.Application):
     
     def __init__(self, **kwargs):
         super(Messenger, self).__init__(**kwargs)
-        self.application_window.create(Contacts, pack_mode="left")
+        self.contacts = self.application_window.create(Contacts, pack_mode="left")
         self.application_window.create("pride.gui.widgetlibrary.Dialog_Box", pack_mode="right",
                                        callback=(self.reference, "send_message"))        
         self.client = self.create(Client, username=self.username, 
@@ -50,4 +68,7 @@ class Messenger(pride.gui.gui.Application):
         assert self.current_contact
         self.client.send_to(self.current_contact, message)
         self.application_window.objects["Dialog_Box"][0].objects["Text_Box"][0].text += "\n" + message
+        
+    def receive_message(self, sender, message):
+        self.alert("{}: {}".format(sender, message), level=self.verbosity.get(sender, 0))
         
