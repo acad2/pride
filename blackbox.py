2d6cb0ca92feecfd9e9fd859e829b2dcd6b31e4a
Ella Rose <erose1337@hotmail.com>
2016-04-12 19:18:15 -0700

    made blackbox service

--- blackbox.py
+++ blackbox.py
@@ -3,10 +3,10 @@ import pride.authentication2
 class Black_Box_Service(pride.authentication2.Authenticated_Service):
     
     defaults = {"input_types" : ("keyboard", "mouse")}
-    remotely_available_procedures = ("handle_user_input", )
+    remotely_available_procedures = ("handle_input", )
     verbosity = {"handle_keyboard" : 0, "handle_mouse" : 0}
     
-    def handle_user_input(self, packed_user_input):
+    def handle_input(self, packed_user_input):
         input_type, input_values = packed_user_input.split(' ', 1)
         if input_type in self.input_types:
             getattr(self, "handle_" + input_type)(input_values)
@@ -23,22 +23,33 @@ class Black_Box_Service(pride.authentication2.Authenticated_Service):
 class Black_Box_Client(pride.authentication2.Authenticated_Client):
                     
     defaults = {"target_service" : "->Python->Black_Box_Service"}
-    verbosity = {"handle_user_input" : 0}
+    verbosity = {"handle_input" : 0, "receive_response" : 0}
     
+    def __init__(self, **kwargs):
+        super(Black_Box_Client, self).__init__(**kwargs)
+        pride.objects["->User->Command_Line"].set_default_program(self.reference, (self.reference, "handle_keyboard"))                 
+        
     @pride.authentication2.remote_procedure_call(callback_name="receive_response")
-    def handle_user_input(self, packed_user_input): 
+    def handle_input(self, packed_user_input): 
         pass
         
     def handle_keyboard(self, input_bytes):
-        self.handle_user_input("keyboard " + input_bytes)
+        self.handle_input("keyboard " + input_bytes)
         
     def handle_mouse(self, mouse_info):
-        self.handle_user_input("mouse " + input_bytes)
-        
-        
+        self.handle_input("mouse " + input_bytes)
+    
+    def receive_response(self, data):
+        self.alert("Received response: {}".format(data), 
+                   level=self.verbosity["receive_response"])
+                   
 def test_black_box_service():
+    try:
+        pride.objects["->User->Command_Line"]
+    except KeyError:
+        pride.objects["->User"].create("pride.shell.Command_Line")
     service = pride.objects["->Python"].create(Black_Box_Service)
-    client = Black_Box_Client(username="localhost")
+    client = Black_Box_Client(username="localhost")    
     
 if __name__ == "__main__":
     test_black_box_service()
