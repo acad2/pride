faaf26ee0c4fbf1dd584cf2ee6138d164cf53c64
Ella Rose <erose1337@hotmail.com>
2016-06-10 15:46:14 -0700

    generalized prp to larger wordsizes

--- crypto/blockcipher2.py
+++ crypto/blockcipher2.py
@@ -1,140 +1,169 @@
-from utilities import xor_sum, rotate_left, rotate_right, slide, xor_subroutine
+from utilities import xor_sum, rotate_left, rotate_right, slide, xor_subroutine, integer_to_bytes, bytes_to_words
     
-def diffusion_transformation(data, diffuser):    
+def prp(data, key, mask=255, rotation_amount=5, bit_width=8):    
     for index in range(len(data)):
         byte = data[index]
-        diffuser ^= byte        
-        data[index] = rotate_left((byte + diffuser + index) % 256, 5)
-        diffuser ^= data[index]            
-    return diffuser
+        key ^= byte                
+        data[index] = rotate_left((byte + key + index) & mask, rotation_amount, bit_width)
+        key ^= data[index]            
+    return key
     
-def bit_transposition(state):
-    output = bytearray(16)
-    for index in range(8):
-        output[index] = 0
-        for index2 in range(8):            
-            byte = state[index2]
-            output[index] |= ((byte & 1) << index2) & 255
-            state[index2] = rotate_right(byte, 1)        
-        
-        output[index + 8] = rotate_left(state[index + 8], 1)
+def prp8(data, key):
+    return prp(data, key)
     
-    state[:] = output[:]  
-
-def byte_transposition(_state):   
-    temp = bytearray(16)
+def prp64(data, key, mask=((2 ** 64) - 1)):            
+    return prp(data, key, mask, 64 - 5, 64)        
     
-    temp[7] = _state[0] 
-    temp[12] = _state[1]
-    temp[14] = _state[2]
-    temp[9] = _state[3]
-    temp[2] = _state[4]
-    temp[1] = _state[5]
-    temp[5] = _state[6]
-    temp[15] = _state[7]
-    temp[11] = _state[8]
-    temp[6] = _state[9]
-    temp[13] = _state[10]
-    temp[0] = _state[11]
-    temp[4] = _state[12]
-    temp[8] = _state[13]
-    temp[10] = _state[14]
-    temp[3] = _state[15]
-            
-    _state[:] = temp[:]
-             
-def decorrelation_layer(data):
-    bit_transposition(data)
-    byte_transposition(data)
-                
-def invert_byte_transposition(state):
-    temp = bytearray(16)
+def invert_prp64(data, key, mask=((2 ** 64) - 1)):
+    return invert_prp(data, key, mask, 64 - 5, 64)
     
-    temp[11] = state[0]
-    temp[5] = state[1]
-    temp[4] = state[2]
-    temp[15] = state[3]
-    temp[12] = state[4]
-    temp[6] = state[5]
-    temp[9] = state[6]
-    temp[0] = state[7]
-    temp[13] = state[8]
-    temp[3] = state[9]
-    temp[14] = state[10]
-    temp[8] = state[11]
-    temp[1] = state[12]
-    temp[10] = state[13]
-    temp[2] = state[14]
-    temp[7] = state[15]
+def test_prp64():
+    data = list(bytearray(16))
+    data[-8] = 1    
+    data[:] = bytes_to_words(data, 8)    
+    _data = data[:]
     
-    state[:] = temp[:]
+    key = prp64(data, xor_sum(data))
+    invert_prp64(data, key)
+    assert _data == data, (data, _data)
     
-def xor_subroutine2(data, key):
-    key_xor = 0
-    data_xor = 0
-    for index, byte in enumerate(key):
-        data_xor ^= data[index]
-        data[index] ^= byte
-        key_xor ^= byte
-    return key_xor, data_xor
+def prf(data, key, mask=255, rotation_amount=5, bit_width=8):
+    new_key = 0
+    for index in range(len(data)):        
+        new_byte = rotate_left((data[index] + key + new_key + index) & mask, rotation_amount, bit_width)
+        new_key ^= new_byte
+        key ^= new_byte
+        data[index] = new_byte
+    return new_key
     
-def encrypt(data, key, rounds=1):
-    key_xor, data_xor = xor_subroutine2(data, key)
-    for round in range(rounds):
-        key_xor = diffusion_transformation(key, key_xor)
-        data_xor = diffusion_transformation(data, data_xor)
-        decorrelation_layer(data)
-        key_xor, data_xor = xor_subroutine2(data, key)
-        
-def invert_diffusion_transformation(data, diffuser):    
-    for index in reversed(range(len(data))):
-        byte = data[index]
-        diffuser ^= byte        
-        data[index] = (256 + (rotate_right(byte, 5) - diffuser - index)) % 256
-        diffuser ^= data[index]
-    return diffuser
-    
-def test_invert_diffusion_transformation():
-    data = bytearray("Testing!")
-    data_xor = diffusion_transformation(data, xor_sum(data))
-    invert_diffusion_transformation(data, data_xor)
-    assert data == "Testing!", data
-    
-def test_diffusion_cycle_length():
-    from utilities import find_cycle_length_subroutine
-    print len(find_cycle_length_subroutine(diffusion_transformation, bytearray("\x00\x01\x00"), 3))
-    
-def test_diffusion_metrics():
-    from utilities import slide, xor_subroutine
+def test_prf():    
     def test_hash(data):
         output = bytearray(16)
+        key = xor_sum(bytearray(data[:16]))
+        for block in slide(bytearray(data), 16):
+            prf(block, xor_sum(block))
+            xor_subroutine(output, block)
+        return bytes(data)
+    from metrics import test_hash_function
+    test_hash_function(test_hash)
+    
+    
+def encrypt(data, key, rounds=1):     
+    key = key[:]
+    round_key = bytearray(16)
+    key_xor = xor_sum(key)
+    data_xor = xor_sum(data)
+    for round in range(rounds):       
+        key_xor = prp(key, key_xor) # generate key        
+        round_key[:] = key[:] # maintain invertible keyschedule
+                
+        round_key_xor = prf(round_key, key_xor) # one way extraction: class 2B keyschedule
+       # data_xor ^= round_key_xor
+       # print "\nRound key:\n", round_key
+       # print "Pre whitening:\n", data
+        xor_subroutine(data, round_key) # pre-whitening                   
+       # print "Applying prp:\n", data, data_xor
+        print "Before prp:\n", data, data_xor
+        data_xor = prp(data, data_xor ^ round_key_xor) # high diffusion prp     
+        print "After prp:\n", data, data_xor
+       # print "Post whitening:\n", data
+        xor_subroutine(data, round_key) # post_whitening
+        
+def decrypt(data, key, rounds=1):
+    round_keys = []
+    key = key[:]
+    round_key = bytearray(16)
+    key_xor = xor_sum(key)
+    data_xor = xor_sum(data)
+    for round in range(rounds):
+        key_xor = prp(key, key_xor)
+        round_key[:] = key[:]
+        round_key_xor = prf(round_key, key_xor)
+        round_keys.append((round_key_xor, round_key[:]))
+    print "\nDecrypting"    
+    for round in reversed(range(rounds)):
+        round_key_xor, round_key = round_keys[round]
+       # print "\nRound key:\n", round_key
+       # print "Removing post whitening\n: ", data
+        xor_subroutine(data, round_key)
+        print "Inverting prp:\n", data, data_xor
+        data_xor = invert_prp(data, data_xor) 
+        print "After inversion:\n", data, data_xor
+    #    print "Removing pre whitening\n: ", data
+        xor_subroutine(data, round_key)
+        #print "    After: \n", data
+       # data_xor ^= round_key_xor
+        
+def test_encrypt_decrypt():
+    data = bytearray(16)
+    plaintext = data[:]
+    key = bytearray(16)
+    rounds = 1
+    
+    encrypt(data, key, rounds)
+    print "Ciphertext: "
+    print data
+    
+    decrypt(data, key, rounds)
+    assert data == plaintext, data
+    
+def invert_prp(data, key, mask=255, rotation_amount=5, bit_width=8):    
+    for index in reversed(range(len(data))):
+        byte = data[index]
+        key ^= byte        
+        data[index] = ((mask + 1) + (rotate_right(byte, rotation_amount, bit_width) - key - index)) & mask
+        key ^= data[index]
+    return key
+    
+def test_invert_prp():
+    data = bytearray("Testing!")
+    data_xor = prp(data, xor_sum(data))
+    invert_prp(data, data_xor)
+    assert data == "Testing!", data
+    
+def test_prp_cycle_length():
+    from utilities import find_cycle_length_subroutine
+    print len(find_cycle_length_subroutine(prp, bytearray("\x00\x01\x00"), 3))
+    
+def test_prp_metrics():
+    from utilities import slide, xor_subroutine
+    def test_hash(data):
+        output = bytearray(16)        
         for block in slide(bytearray(data), 16):            
-            diffusion_transformation(block)
+            prp(block, xor_sum(block))
             xor_subroutine(output, block)
         return bytes(output)
         
     from metrics import test_hash_function
     test_hash_function(test_hash)
+        
+def test_prp_s_box():  
+    import collections
     
-def test_bit_byte_transposition_diffusion():
-    def test_hash(data):
-        output = bytearray(16)
-        for block in slide(bytearray(data), 16):
-            block.extend("\x00" * (16 - len(block)))
-            assert len(block) == 16
-            for round in range(4):
-                bit_transposition(block)
-                byte_transposition(block)
-                for index in range(16):
-                    block[index] = (block[index] + block[(index + 2) % 16] + index) % 256
-            xor_subroutine(output, block)        
-        return bytes(output)
-    from metrics import test_hash_function
-    test_hash_function(test_hash)
+    s_box = collections.defaultdict(bytearray)
+    rounds = 4
+    for ending in range(256):
+        data = bytearray(16)
+        data[-1] = ending
+        for round in range(rounds):            
+            prp(data, xor_sum(data))                  
+            s_box[round].append(data[-2])
+    
+    from differential import find_best_differential
+    from linear import calculate_linearity
+
+    for round in range(rounds):
+        _s_box = s_box[round]
+        print "Best differential after {} rounds: ".format(round + 1), find_best_differential(_s_box)
+        print "Linearity after {} rounds: ".format(round + 1), calculate_linearity(_s_box)
+    
     
 if __name__ == "__main__":
-    test_invert_diffusion_transformation()
-    #test_diffusion_cycle_length()
-    #test_diffusion_metrics()
-    #test_bit_byte_transposition_diffusion()
+    #test_invert_prp()
+    #test_prp_cycle_length()
+    #test_prp_metrics()    
+    #test_prp_s_box()
+    #test_encrypt_decrypt()
+    #test_prf()
+    test_prp64()
     
