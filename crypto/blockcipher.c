625c02ae057502180121bc90adef9d84b5d7e954
Ella Rose <erose1337@hotmail.com>
2016-06-11 15:51:33 -0700

    changed memcpy to memcpy_s

--- crypto/blockcipher.c
+++ crypto/blockcipher.c
@@ -1,6 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
+#ifndef memcpy_s
+void memcpy_s(unsigned char* s1, unsigned char* s2, size_t n)
+{
+    int index;
+    for (index = 0; index < n; index++)
+    {
+        s1[index] = s2[index];
+    }
+}
+#endif
+
 void print_data(unsigned char* data)
 {
     int index;
@@ -11,24 +23,24 @@ void print_data(unsigned char* data)
     }
 }   
 
-unsigned char rotate_left(unsigned char byte, int amount)
+unsigned char rotate_left(unsigned char word8, int amount)
 {    
-    return ((byte << amount) | (byte >> (8 - amount))) & 255;
+    return ((word8 << amount) | (word8 >> (8 - amount))) & 255;
 }
 
-unsigned char rotate_right(unsigned char byte, int amount)
+unsigned char rotate_right(unsigned char word8, int amount)
 {              
-    return ((byte >> amount) | (byte << (8 - amount))) & 255;
+    return ((word8 >> amount) | (word8 << (8 - amount))) & 255;
 }
 
 int prp(unsigned char* data, unsigned char key, unsigned char data_size)
 {
-    unsigned char index, byte;
+    unsigned char index, data_byte;
     for (index = 0; index < data_size; index++)
     {    
-        byte = data[index];
-        key ^= byte;                       
-        data[index] = rotate_left((byte + key + index) & 255, 5);        
+        data_byte = data[index];
+        key ^= data_byte;                       
+        data[index] = rotate_left((data_byte + key + index) & 255, 5);        
         key ^= data[index]; 
     }
     return key;
@@ -74,7 +86,7 @@ void encrypt(unsigned char* data, unsigned char* _key, int rounds)
     for (index = 0; index < rounds; index++)
     {                
         key_xor = prp(key, key_xor, 16); // generate key 
-        memcpy(round_key, key, 16); // maintain invertible keyschedule                
+        memcpy_s(round_key, key, 16); // maintain invertible keyschedule                
         
         prf(round_key, key_xor, 16); // one way extraction: class '2B' keyschedule        
         
@@ -116,15 +128,15 @@ void decrypt(unsigned char* data, unsigned char* _key, int rounds)
     for (index = 0; index < rounds; index++)
     {          
         key_xor = prp(key, key_xor, 16);        
-        memcpy(round_key, key, 16);
+        memcpy_s(round_key, key, 16);
                 
         prf(round_key, key_xor, 16);
-        memcpy(round_keys + (index * 16), round_key, 16);                
+        memcpy_s(round_keys + (index * 16), round_key, 16);                
     }
     
     for (index = (rounds - 1); index != -1; index--)
     {            
-        memcpy(round_key, round_keys + (index * 16), 16);
+        memcpy_s(round_key, round_keys + (index * 16), 16);
         
         data_xor = xor_with_key(data, round_key);         
         data_xor = invert_prp(data, data_xor, 16);
@@ -138,10 +150,10 @@ void test_encrypt_decrypt()
     unsigned char data[16], key[16], plaintext[16];
     int rounds = 16, index;
     
-    memcpy(data, null_string, 16);       
+    memcpy_s(data, null_string, 16);       
     data[15] = 1;
-    memcpy(plaintext, data, 16);
-    memcpy(key, null_string, 16); 
+    memcpy_s(plaintext, data, 16);
+    memcpy_s(key, null_string, 16); 
     
     encrypt(data, key, rounds);
     
