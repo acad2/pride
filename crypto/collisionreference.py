f077a7a1ff7fe92d7cc35b79bcf2759e28f75a99
Ella Rose <erose1337@hotmail.com>
2016-07-06 13:35:38 -0700

    added sponge tests

--- crypto/collisionreference.py
+++ crypto/collisionreference.py
@@ -1,5 +1,6 @@
 from blockcipher2 import prp as old_prp
-from streamcipher2 import prp as new_prp
+from streamcipher2 import prp as new_prp, xor_sum
+from sponge import sponge_factory
 
 def test_for_collisions():
     data1 = bytearray(16)
@@ -27,6 +28,23 @@ def test_for_collisions():
     print "data3:\n", data3
     print    
     print "data4:\n", data4
+    
+    hasher1 = sponge_factory(lambda data: old_prp(data, xor_sum(data)), rate=8, capacity=8)
+    hasher2 = sponge_factory(lambda data: new_prp(data, xor_sum(data), key_slice=None), rate=8, capacity=8)
+    
+    null_string_hash = hasher1('')
+    zero_hash = hasher1('0')
+    
+    null_string_hash2 = hasher2('')
+    zero_hash2 = hasher2('0')
+    
+    print "Sponge outputs with old prp: "
+    print "Null string hash:\n", null_string_hash
+    print "\nZero hash:\n", zero_hash
+    
+    print "Sponge outputs with new prp: "
+    print "Null string hash:\n", null_string_hash2
+    print "\nZero hash:\n", zero_hash2
     
 if __name__ == "__main__":
     test_for_collisions()   
