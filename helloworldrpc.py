81c1252e27fc07908d97c83a1d24401b151af59e
Ella Rose <python_pride@protonmail.com>
2016-07-23 12:26:34 -0700

    added argument demo

--- helloworldrpc.py
+++ helloworldrpc.py
@@ -4,8 +4,8 @@ class Rpc_Hello_World_Service(pride.authentication2.Authenticated_Service):
     
     remotely_available_procedures = ("hello_world", )
     
-    def hello_world(self):
-        return "Hello world"
+    def hello_world(self, argument, *args, **kwargs):                
+        return "Hello world" + ' ' + argument
         
 
 class Rpc_Hello_World_Client(pride.authentication2.Authenticated_Client):
@@ -13,14 +13,14 @@ class Rpc_Hello_World_Client(pride.authentication2.Authenticated_Client):
     defaults = {"target_service" : "/Rpc_Hello_World_Service"}
     
     @pride.authentication2.remote_procedure_call(callback_name="alert")
-    def hello_world(self):
+    def hello_world(self, argument, *args, **kwargs):
         pass
         
 def test_hello_world_rpc():
     service = Rpc_Hello_World_Service()
     client = Rpc_Hello_World_Client(username="Ella")
     
-    client.hello_world()
+    client.hello_world("I'm an argument!", "I work with any number of arguments", 1, 2, 3, testing=True)
     
 if __name__ == "__main__":
     test_hello_world_rpc()
