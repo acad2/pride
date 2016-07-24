d321f9a9dfd74c44b93b1572838e548913d7f565
Ella Rose <python_pride@protonmail.com>
2016-07-23 21:08:13 -0700

    added cached keyschedule variant

--- crypto/blockcipher3.c
+++ crypto/blockcipher3.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <windows.h>
+
 #ifndef memcpy_s
 
 #define ROTATION_AMOUNT 5
 #define BIT_WIDTH 8
+#define DATA_SIZE 16
 
 void memcpy_s(unsigned char* s1, unsigned char* s2, size_t n)
 {
@@ -37,43 +40,43 @@ unsigned char rotate_right(unsigned char word8, int amount)
     return ((word8 >> amount) | (word8 << (8 - amount)));
 }
 
-unsigned char round_function(unsigned char* state, unsigned char key, int index)
+unsigned char round_function(unsigned char* state, unsigned char key, int left_index, int right_index)
 {
     unsigned char left, right;
-    left = state[index - 1];
-    right = state[index];
+    left = state[left_index];
+    right = state[right_index];
     
     key ^= right;               
-    right = rotate_left(right + key + index, ROTATION_AMOUNT);
+    right = rotate_left(right + key + left_index, ROTATION_AMOUNT);
     key ^= right;
     
     key ^= left;       
     left += right >> (BIT_WIDTH / 2);                
-    left ^= rotate_left(right, (index % BIT_WIDTH) ^ ROTATION_AMOUNT);                    
+    left ^= rotate_left(right, (left_index % BIT_WIDTH) ^ ROTATION_AMOUNT);                    
     key ^= left;
     
-    state[index - 1] = left;
-    state[index] = right;
+    state[left_index] = left;
+    state[right_index] = right;
     return key;    
 }
     
-unsigned char invert_round_function(unsigned char* state, unsigned char key, int index)
+unsigned char invert_round_function(unsigned char* state, unsigned char key, int left_index, int right_index)
 {
     unsigned char left, right;
-    left = state[index - 1];
-    right = state[index];
+    left = state[left_index];
+    right = state[right_index];
     
     key ^= left;
-    left ^= rotate_left(right, (index % BIT_WIDTH) ^ ROTATION_AMOUNT);
+    left ^= rotate_left(right, (left_index % BIT_WIDTH) ^ ROTATION_AMOUNT);
     left -= right >> (BIT_WIDTH / 2);
     key ^= left;
     
     key ^= right;
-    right = rotate_right(right, ROTATION_AMOUNT) - (key + index);
+    right = rotate_right(right, ROTATION_AMOUNT) - (key + left_index);
     key ^= right;
     
-    state[index - 1] = left;
-    state[index] = right;
+    state[left_index] = left;
+    state[right_index] = right;
     return key;
 }
         
@@ -110,8 +113,10 @@ int prp(unsigned char* data, unsigned char key, unsigned char data_size)
     
     for (index = data_size - 1; index != 0; index--)
     {
-        key = round_function(data, key, index);
+        key = round_function(data, key, index - 1, index);
     }       
+    
+    key = round_function(data, key, data_size - 1, 0);
     return key;
 }
         
@@ -123,14 +128,16 @@ void prf(unsigned char* data, unsigned char key, unsigned char data_size)
     for (index = data_size - 1; index != 0; index--)
     {        
         key ^= data[index]; // remove so that the first right ^ key in round_function will reinsert it        
-        key = round_function(data, key, index);        
+        key = round_function(data, key, index - 1, index);        
     }
+    key = round_function(data, key, data_size - 1, 0);
 }
     
 unsigned char xor_with_key(unsigned char* data, unsigned char* key)
 {
     unsigned char data_xor = 0, index;    
-    
+    //printf("Xor with key\n");
+    //print_data(key);
     for (index = 0; index < 16; index++)
     {           
         data[index] ^= key[index];
@@ -139,9 +146,19 @@ unsigned char xor_with_key(unsigned char* data, unsigned char* key)
     return data_xor;
 }
 
-void key_schedule(unsigned char* round_keys, unsigned char* round_key, unsigned char* key, unsigned char key_xor, int rounds)
+void key_schedule(unsigned char* round_keys, unsigned char* _key, int rounds)
 {
     int index;
+    unsigned char key_byte, key_xor = 0;
+    unsigned char key[16], round_key[16];
+            
+    for (index = 0; index < 16; index++) // create working copy of the key
+    {
+        key_byte = _key[index];
+        key[index] = key_byte;
+        key_xor ^= key_byte;                                        
+    }
+    
     for (index = 0; index < rounds + 1; index++) // key schedule
     {          
         key_xor = prp(key, key_xor, 16);        
@@ -152,20 +169,13 @@ void key_schedule(unsigned char* round_keys, unsigned char* round_key, unsigned 
     }
 }
     
-void encrypt(unsigned char* data, unsigned char* _key, int rounds)
-{
-    unsigned char key[16];
+void encrypt(unsigned char* data, unsigned char* key, int rounds)
+{    
     unsigned char round_keys[16 * (rounds + 1)];
     unsigned char round_key[16], key_xor = 0, data_xor = 0, key_byte;
     int index;
-    for (index = 0; index < 16; index++) // create working copy of the key
-    {
-        key_byte = _key[index];
-        key[index] = key_byte;
-        key_xor ^= key_byte;                                        
-    }
-        
-    key_schedule(round_keys, round_key, key, key_xor, rounds);
+    
+    key_schedule(round_keys, key, rounds);
     
     for (index = 0; index < rounds; index++) // iterated even-mansour construction
     {                            
@@ -178,6 +188,23 @@ void encrypt(unsigned char* data, unsigned char* _key, int rounds)
     xor_with_key(data, round_key);
     
 }
+
+void encrypt_cached_keyschedule(unsigned char* data, unsigned char* round_keys, int rounds)
+{        
+    unsigned char round_key[16], data_xor = 0;
+    int index;
+            
+    for (index = 0; index < rounds; index++) // iterated even-mansour construction
+    {                            
+        memcpy_s(round_key, round_keys + (index * 16), 16);    
+                
+        data_xor = xor_with_key(data, round_key);        
+        prp(data, data_xor, 16);
+    }    
+    memcpy_s(round_key, round_keys + (rounds * 16), 16);    
+    xor_with_key(data, round_key);    
+}
+
 
 void invert_shuffle_bytes(unsigned char* state)
 {
@@ -207,38 +234,26 @@ unsigned char invert_prp(unsigned char* data, unsigned char key, int data_size)
 {       
     int index;    
     
+    key = invert_round_function(data, key, data_size - 1, 0);
+    
     for (index = 1; index < data_size; index++)
     {
-        key = invert_round_function(data, key, index);
+        key = invert_round_function(data, key, index - 1, index);
     }
     invert_shuffle_bytes(data);
     return key;
 }
     
-void decrypt(unsigned char* data, unsigned char* _key, int rounds)
+void decrypt(unsigned char* data, unsigned char* key, int rounds)
 {
-    unsigned char key[16], round_key[16];
+    unsigned char round_key[16];
     unsigned char round_keys[16 * (rounds + 1)];
-    unsigned char key_xor = 0, data_xor = 0, key_byte;
+    unsigned char data_xor = 0;
     int index;
-    for (index = 0; index < 16; index++)
-    {
-        key_byte = _key[index];
-        key_xor ^= key_byte;
-        key[index] = key_byte;                
-    }      
     
-    //for (index = 0; index < rounds + 1; index++)
-    //{          
-    //    key_xor = prp(key, key_xor, 16);        
-    //    memcpy_s(round_key, key, 16);
-    //            
-    //    prf(round_key, key_xor, 16);
-    //    memcpy_s(round_keys + (index * 16), round_key, 16);                        
-    //}
+    key_schedule(round_keys, key, rounds);    
     
-    key_schedule(round_keys, round_key, key, key_xor, rounds);    
-        
+    memcpy_s(round_key, round_keys + (rounds * 16), 16);    
     data_xor = xor_with_key(data, round_key);         
     
     for (index = rounds; index--;)
@@ -252,13 +267,12 @@ void decrypt(unsigned char* data, unsigned char* _key, int rounds)
   
 void test_encrypt_decrypt()
 {    
-    unsigned char data[16], key[16], plaintext[16], null_string[16];
+    unsigned char data[16], key[16], null_string[16];
     int rounds = 2;
     
     memset(null_string, 0, 16);
     memcpy_s(data, null_string, 16);       
-    data[15] = 1;
-    memcpy_s(plaintext, data, 16);
+    data[15] = 1;    
     memcpy_s(key, null_string, 16); 
     
     printf("Encrypting...\n");
@@ -269,8 +283,50 @@ void test_encrypt_decrypt()
  //       
     decrypt(data, key, rounds);
     print_data(data);
+    
+    unsigned char round_keys[(rounds + 1) * 16];
+    key_schedule(round_keys, key, rounds);
+    
+    encrypt_cached_keyschedule(data, round_keys, rounds);
+    printf("Data: %s\n", data);
+    print_data(data);
+    
 }
 
+
+//void test_encrypt_performance()
+//{
+//    LARGE_INTEGER ticksPerSecond = 0, ticksLastTime = 0, ticksThisTime = 0;
+//    QueryPerformanceFrequency(&ticksPerSecond);
+//    
+//    //QueryPerformanceCounter(&ticksLastTime);        
+//    //QueryPerformanceCounter(&ticksThisTime);
+//    //long long seconds = (ticksThisTime - ticksLastTime) / ticksPerSecond;
+//
+//        
+//    int rounds = 1;
+//    int blocks = 1;    
+//    unsigned char key[16], round_keys[rounds * 16], round_key[16];
+//    
+//	WORD* data = (WORD*)malloc(DATA_SIZE * blocks * sizeof(WORD));
+//	WORD* plaintext = (WORD*)malloc(DATA_SIZE * blocks * sizeof(WORD));
+//
+//	// setup test data
+//	memset(data, 16, DATA_SIZE * blocks * sizeof(WORD));
+//	memcpy(plaintext, data, sizeof(data));
+//	memset(key, 1, sizeof(key));
+//	
+//	key_schedule(round_keys, key, rounds);    
+//    encrypt_cached_keyschedule(round_keys)
+//}   
+
+
+        
+        
+        
+        
+        
+    
 int main()
 {
     test_encrypt_decrypt();
